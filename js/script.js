f8f77f6b84cf16f089a2f5bdf531e6fbc9f86d41
My Name <myEmail@example.com>
2023-02-13 12:43:39 +0100

    home-work15

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const burgerMenu = document.querySelector('.check'); 
+const body = document.querySelector('.body'); 
+ 
+burgerMenu.addEventListener('change', function(event) { 
+    if (event.target.checked) { 
+        body.classList.add('lock') 
+    } 
+    else { 
+        body.classList.remove('lock') 
+    } 
+})
+
+$('#teamSlider').slick({
+    dots: true
+});
